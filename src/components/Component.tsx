77c0d3c17c76445c5ede477837ffda711d4e3d93
17u5h <102058870+17u5h@users.noreply.github.com>
2023-09-06 18:14:30 +0700

    initial commit

---
+++ src/components/Component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './App.module.scss'
+
+const Component = () => {
+	return (
+		<div style={{height: '100vh'}} className={classes.appComponent}>
+			ulbi
+		</div>
+	);
+};
+
+export default Component;
